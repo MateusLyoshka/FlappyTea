b419b33dbef14a2e225ee6cc68a0cca1b37fca21
MateusLyoshka <mateusbigbang@gmail.com>
2025-04-25 04:00:26 -0300

    Game over added

--- Assets/cup_script.cs
+++ Assets/cup_script.cs
@@ -4,18 +4,26 @@ public class cup_script : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
     public float flapStrength;
+    public logic_script logic;
+    public bool cupIsAlive = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        logic = GameObject.FindGameObjectWithTag("logic").GetComponent<logic_script>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) == true && cupIsAlive)
         {
             myRigidBody.linearVelocity = Vector2.up * flapStrength;
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        cupIsAlive = false;
     }
 }
--- Assets/logic_script.cs
+++ Assets/logic_script.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class logic_script : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase score")]
     public void addScore(int scoreToAdd)
     {
         playerScore += scoreToAdd;
         scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
 }
